907afc757f219b1c3faa4749c22f7ca6ed2b98e2
shehrozchishti0336 <shehrozchishtinn2@gmail.com>
2021-06-14 10:21:27 +0500

    comiiting to sub folder

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 echo '<h1>This is my first Git Project</h1>';
 
 
-echo '<h1>Changed this file online</h1>';
+echo '<h1>Changed this file online for edit</h1>';
 
 echo "from desktop app";
+
+echo "Created a branch";
 ?>
